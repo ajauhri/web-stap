4128615531f4f7cf9323932891f58b49188bfc7b
erikreed <ereed.uw@gmail.com>
2013-03-30 17:50:11 -0400

    removed page load timeout

--- crawler.py
+++ crawler.py
@@ -8,16 +8,11 @@ MAX_TIMEOUT = 20000 # milliseconds
 
 sites = open(SITES_LIST, 'r').read().split('\n')
 
-
 for i, site in enumerate(sites[:MAX_SITES]):
-  print "[%d of %d] Loading site: %s" % (i, MAX_SITES, site)
+  print "[%d of %d] Loading site: %s" % (i+1, MAX_SITES, site)
   browser = webdriver.Firefox() # Get local session of firefox
   tstart = time()
   browser.get("http://www.yahoo.com") # Load page
-  try:
-    browser.wait_for_page_to_load(MAX_TIMEOUT)
-  except Exception as e:
-    print "Page never finished loading! %s" % str(e)
   tend = time()
   
   print "Page load time: %.2f seconds" % (tend - tstart)
