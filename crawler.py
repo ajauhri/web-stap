003b702b380e1dd0e796e8124f1713d6cea4f7a8
erikreed <ereed.uw@gmail.com>
2013-03-30 23:33:25 -0400

    adjusted stap rates

--- crawler.py
+++ crawler.py
@@ -12,8 +12,8 @@ MEASURING_SCRIPT1 = './nettop.stp'
 MEASURING_SCRIPT2 = './syscall.stp'
 MOBILE_UA = 'Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Build/GRI40) ' + \
     'AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile'
-MAX_SITES = 10
-SECONDS_PER_SITE = 60
+MAX_SITES = 1
+SECONDS_PER_SITE = 150
 
 def main():
   if os.getuid() != 0:
@@ -30,16 +30,17 @@ def main():
       profile = webdriver.FirefoxProfile()
       if mobile:
         profile.set_preference("general.useragent.override", MOBILE_UA)
+        site += '-m'
       browser = webdriver.Firefox(profile)
       sleep(1)
 
       tstart = time()
-      pStap1 = Popen('%s > output/%s.stap-packets.csv' % (MEASURING_SCRIPT1, site), \
+      pStap1 = Popen('%s > output/%s-stap-packets.csv' % (MEASURING_SCRIPT1, site), \
           stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
-      pStap2 = Popen('%s > output/%s.stap-syscalls.csv' % (MEASURING_SCRIPT2, site), \
+      pStap2 = Popen('%s > output/%s-stap-syscalls.csv' % (MEASURING_SCRIPT2, site), \
           stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
       pConn = Popen(r'watch -n .5 "date; netstat -an ' + \
-          '| grep ESTABLISHED | wc -l >> output/%s.conn.csv"' % site, \
+          '| grep ESTABLISHED | wc -l >> output/%s-conns.csv"' % site, \
           stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
       browser.get(site_full) # Load page
       tend = time()
