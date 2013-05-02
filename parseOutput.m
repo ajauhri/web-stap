366fd965f6d1423e24fdfa7f38b167ed65ca57a6
erikreed <ereed.uw@gmail.com>
2013-05-02 17:07:52 -0400

    better looking figures; plotting mobile

--- parseOutput.m
+++ parseOutput.m
@@ -61,8 +61,9 @@ for i=length(sites):-1:1
    end
    loadtimeM = str2num(out);
    
-   % if dom never loaded...
-   if loadtime(2) < 0 || loadtimeM(2) < 0
+   % if dom never loaded, or if page loaded abnormally fast...
+   if loadtime(2) < 0 || loadtimeM(2) < 0 || ... 
+           loadtime(1) <= 0 || loadtimeM(1) <= 0
        fprintf('Skipping %s (no page load) \n', sites{i});
        sites(i) = [];
        continue
@@ -71,7 +72,9 @@ for i=length(sites):-1:1
    fname = sprintf('output/%s-stap.csv.bz2', sites{i});
    cmd = sprintf( ...
        ['bash -c "bzcat %s | grep -v ''browser:''' ...
-       ' | sed -e ''s/[^0-9~]*//g'' -e ''s/~/,/g''' ...
+       ' | sed  -e ''s/JS Sour~ Thread/Sour Thread/g'' ' ...
+       '-e ''s/Proxy R~olution/Proxy Rolution/g'' -e ' ...
+       '''s/[^0-9~]*//g'' -e ''s/~/,/g''' ...
        ' -e ''s/,,/,-1,/g'' -e ''s/,$//'' > .tmp"'], fname);
    [status, out] = system(cmd);
    if status ~= 0
@@ -82,10 +85,13 @@ for i=length(sites):-1:1
    staps = importdata('.tmp');
    
    fname = sprintf('output/%s-m-stap.csv.bz2', sites{i});
-   [status, out] = system(sprintf( ...
+   cmd = sprintf( ...
        ['bash -c "bzcat %s | grep -v ''browser:''' ...
-       ' | sed -e ''s/[^0-9~]*//g'' -e ''s/~/,/g''' ...
-       ' -e ''s/,,/,-1,/g'' -e ''s/,$//''  > .tmp"'], fname));
+       ' | sed  -e ''s/JS Sour~ Thread/Sour Thread/g'' ' ...
+       '-e ''s/Proxy R~olution/Proxy Rolution/g'' -e ' ...
+       '''s/[^0-9~]*//g'' -e ''s/~/,/g''' ...
+       ' -e ''s/,,/,-1,/g'' -e ''s/,$//'' > .tmp"'], fname);
+   [status, out] = system(cmd);
    if status ~= 0
        fprintf('Skipping %s (incomplete) \n', sites{i});
        sites(i) = [];
@@ -94,7 +100,7 @@ for i=length(sites):-1:1
    stapsM = importdata('.tmp');
    
    if length(conns) < 250 || length(connsM) < 250 || isempty(staps) || isempty(stapsM)
-       fprintf('Skipping %s (Odd output)\n', sites{i});
+       fprintf('Skipping %s (odd output)\n', sites{i});
        sites(i) = [];
       continue 
    end
@@ -115,6 +121,7 @@ for i=length(sites):-1:1
    allStaps = [allStaps; {staps}];
    allStapsM = [allStapsM; {stapsM}];
 end
+save parsed
 
 %% link stap indices; remove empty sites
 load stap_dim
@@ -122,12 +129,16 @@ stapTypes = length(stap_dim);
 numSites = length(allStaps);
 
 stapDataAggregated = cell(numSites, stapTypes);
+stapDataAggregatedM = cell(numSites, stapTypes);
 
 for i=1:numSites
     staps = allStaps{i};
     for j=1:stapTypes
        relevantStaps = staps(staps(:,1) == j, 2:length(stap_dim{j})+1);
        stapDataAggregated{i,j} = relevantStaps;
+       
+       relevantStapsM = stapsM(stapsM(:,1) == j, 2:length(stap_dim{j})+1);
+       stapDataAggregatedM{i,j} = relevantStapsM;
     end
 end
 
@@ -145,35 +156,56 @@ bytesSent = relevantStap(:,end-1);
 plot(cumsum(bytesRec))
 
 %% plot staps (all sites)
+save_figs = true;
+close all;
 mkdir('figs');
-for i=1:numSites
+for i=1:1
     sitename = sites(length(sites) - i + 1);
+    mkdir(sprintf('figs/%s',sitename{:}));
     for j=1:stapTypes
        relevantStap = stapDataAggregated{i, j};
+       relevantStapM = stapDataAggregatedM{i, j};
        % [process_name, PID, timestep, ...]
        timestamps = relevantStap(:,3);
-       [sortedTimestamps, sortedInd] = sort(timestamps);
+       timestampsM = relevantStapM(:,3);
+
        for k=4:length(stap_dim{j})
            feature_names = stap_dim{j};
-           stap_time_series = relevantStap(:,k);
+           stap_time_series = sortrows([timestamps relevantStap(:,k)]);
+           stap_time_seriesM = sortrows([timestampsM relevantStapM(:,k)]);
            subplot(2,1,1)
-           plot(sortedTimestamps,stap_time_series(sortedInd))
+           plot(stap_time_series(:,1),stap_time_series(:,2), ...
+               'Linewidth', 1, 'MarkerSize',4, 'Color',[0.306 0.396 0.580])
+           hold all
+           plot(stap_time_seriesM(:,1),stap_time_seriesM(:,2), ...
+               '--','Linewidth', 1, 'MarkerSize',4,'Color',[0.043 0.518 0.780])
+           box off
            ylabel(feature_names{k})
            title(sprintf('%s -- %s', sitename{:}, feature_names{k}))
+           legend('Desktop UA' ,'Mobile UA')
            
            subplot(2,1,2)
-           plot(sortedTimestamps,cumsum(stap_time_series(sortedInd)))
+           plot(stap_time_series(:,1),cumsum(stap_time_series(:,2)), ...
+               'Linewidth', 1, 'MarkerSize',4, 'Color',[0.306 0.396 0.580])
+           hold all
+           plot(stap_time_seriesM(:,1),cumsum(stap_time_seriesM(:,2)), ...
+               '--','Linewidth', 1, 'MarkerSize',4,'Color',[0.043 0.518 0.780])
+           box off
            ylabel('Cumulative sum')
            xlabel('Time (seconds)')
+           legend('Desktop UA' ,'Mobile UA')
            
-           set(gcf,'PaperPositionMode','auto')
-           print(gcf,'-dpng','-r300', sprintf('figs/%s.%d.%s.png', ...
-               sitename{:}, j, feature_names{k}))
-           close all
+           if save_figs
+               set(gcf,'PaperPositionMode','auto')
+               print(gcf,'-dpng','-r300', sprintf('figs/%s/%d.%s.png', ...
+                   sitename{:}, j, feature_names{k}))
+           end
+%            pause
+           clf('reset') 
        end
     end
 end
-
+close all
 
 %%
 
