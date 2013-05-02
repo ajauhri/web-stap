301e78e16292a06568c15f22bc450c3f405d46ad
ajauhri <abhinavjauhri@gmail.com>
2013-05-02 04:45:22 -0400

    nw corrected to probe for wlan only

--- crawler.py
+++ crawler.py
@@ -14,7 +14,7 @@ MOBILE_UA = 'Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Buil
     'AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile'
 SECONDS_PER_SITE = 150
 MAX_SITES = 1e6
-START_INDEX = 0
+START_INDEX = 259
 
 def main():
   if os.getuid() != 0:
@@ -23,7 +23,7 @@ def main():
   sites = open(SITES_LIST, 'r').read().split('\n')
   maxSites = min(MAX_SITES, len(sites))
 
-  os.system('mkdir -p output')
+  os.system('mkdir -p output_corr')
   for i, site in enumerate(sites[START_INDEX:maxSites]):
     site_full = 'http://' + site
     print "[%d of %d] Loading site: %s" % (i+1+START_INDEX, maxSites, site_full)
@@ -36,14 +36,14 @@ def main():
       browser = webdriver.Firefox(profile)
       browser.set_page_load_timeout(SECONDS_PER_SITE)
       browserPID = browser.binary.process.pid
-      sleep(10)
-      cmd = '%s -G parent_id=%s -G browser_id=%s > output/%s-stap.csv' % \
+      sleep(5)
+      cmd = '%s -G parent_id=%s -G browser_id=%s > output_corr/%s-stap.csv' % \
           (MEASURING_SCRIPT, str(os.getpid()), str(browserPID), site)
       print cmd
       pStap = Popen(cmd, \
           stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
       pConn = Popen('watch -n .2 "bash measure-connections.sh >> ' \
-          'output/%s-conns.csv"' % site, \
+          'output_corr/%s-conns.csv"' % site, \
           stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
       def close(signal, frame):
         print 'Caught sigint--terminating.'
@@ -72,14 +72,14 @@ def main():
         )
         print "Page load timers:"
         for i in timing: print ' * %s: %dms' % (i, timing[i])
-        with open('output/%s-loadtime.csv' % site, 'w') as f:
+        with open('output_corr/%s-loadtime.csv' % site, 'w') as f:
           f.write(','.join(str(i) for i in timing.values()))
       except (WebDriverException, TypeError) as e:
         print 'Javascript timer error: ' + str(e)
       kill((pConn, pStap))
       browser.quit()
       # since the files are getting somewhat large, ~3-5MB, compress them
-      os.system('bzip2 -f output/*.csv')
+      os.system('bzip2 -f output_corr/*.csv')
     print
   print "Terminated successfully!"
 
