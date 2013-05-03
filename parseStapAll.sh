af8344787351ae80f8b912d7e70ff5f55a763fbf
erikreed <ereed.uw@gmail.com>
2013-05-02 23:09:03 -0400

    new naming convention

--- parseStapAll.sh
+++
@@ -1,5 +0,0 @@
-bzcat $1 | grep -v 'browser:' | \
-    sed  -e 's/JS Sour~ Thread/Sour Thread/g' \
-    -e 's/Proxy R~olution/Proxy Rolution/g' \
-    -e 's/[^0-9~]*//g' -e 's/~/,/g' -e 's/,,/,-1,/g' -e 's/,$//' \
-    > .tmp
